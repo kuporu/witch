721457094fc5b03759c3462db0ec7bafc0a296b6
hgc <3292645343@qq.com>
2022-01-07 13:50:33 +0800

    modify type of SqlCommandType

--- src/main/java/org/Archibald/medator/SqlContext.java
+++ src/main/java/org/Archibald/medator/SqlContext.java
@@ -1,12 +1,14 @@
 package org.Archibald.medator;
 
+import org.Archibald.mapping.SqlCommandType;
+
 import java.util.Map;
 
 public class SqlContext {
     private String sql;                                             // 预编译后的sql语句
     private String resultType;                                      // sql语句执行返回的包装结果类型
     private Map<Integer, String> locationAndPlaceholderName;        // 预编译后的sql语句占位符位置和名字
-    private String SqlCommandType;                                  // CRUD类型
+    private SqlCommandType sqlCommandType;                                  // CRUD类型
 
     public String getSql() {
         return sql;
@@ -32,11 +34,11 @@ public class SqlContext {
         this.locationAndPlaceholderName = locationAndPlaceholderName;
     }
 
-    public String getSqlCommandType() {
-        return SqlCommandType;
+    public SqlCommandType getSqlCommandType() {
+        return sqlCommandType;
     }
 
-    public void setSqlCommandType(String sqlCommandType) {
-        SqlCommandType = sqlCommandType;
+    public void setSqlCommandType(SqlCommandType sqlCommandType) {
+        this.sqlCommandType = sqlCommandType;
     }
 }
